54a9618cc106392d4c8faf2bcbcde6802eae6cdd
Eduard Nitu <edi.nitu2000@yahoo.com>
2026-04-22 09:28:54 +0200

    auth: keycloak for TUG/qa (#79)
    * Revert "review feedback"
    
    This reverts commit 0babb7ce11585a119c6ebbaf0f2e76f2b0bf5b8e.
    
    * Revert "fix: update template & cfg"
    
    This reverts commit fde12eab9fa40145186ae5ee3b059b99918b6fe4.
    
    * Revert "feat: tug qa saml"
    
    This reverts commit 56e31bd2373c7598886e751c43e32546f06a5bd9.
    
    * auth: TUG/qa keycloak
    
    * review feedback

--- auth/yaml2py.py
+++ auth/yaml2py.py
@@ -1,10 +1,12 @@
 """
 Script that injects a given yaml config into arguments of an invenio.cfg class.
+
+Usage python yaml2py --source-filename your_config.yaml --dest-filename your_invenio.cfg --node rdm --placeholder <inject_config_here>
 """
 
 import argparse
 import sys
-
+import os
 import yaml
 
 parser = argparse.ArgumentParser()
@@ -16,13 +18,23 @@ parser.add_argument("--placeholder", type=str, required=True)
 
 args = parser.parse_args()
 
+secrets = {}
+secret_env = os.environ.get("YAML2PY_SECRETS")
+if secret_env:
+    secrets_list = secret_env.split(",")
+    for one_secret_str in secrets_list:
+        key_val = one_secret_str.split("=")
+        secrets[key_val[0].lower()] = key_val[1]
+
 auth_config = ""
 with open(args.source_filename) as f:
     data = yaml.safe_load(f)
     for key, _ in data.items():
         if key == args.node:
             for node_key, val in data[key].items():
-                if isinstance(val, str):
+                if isinstance(val, str) and val == "<secret>":
+                    auth_config += f'{node_key}="{secrets[node_key]}",\n'
+                elif isinstance(val, str):
                     auth_config += f'{node_key}="{val}",\n'
                 else:
                     auth_config += f"{node_key}={val},\n"
