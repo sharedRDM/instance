7d4a1f020e9fa22b86a88bc91573d6588f150c46
Eduard Nitu <edi.nitu2000@yahoo.com>
2026-08-21 12:05:10 +0200

    fix: cleanup instance

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -1,17 +1,10 @@
-// This file is part of InvenioRDM
-// Copyright (C) 2023 CERN.
-//
-// Invenio App RDM is free software; you can redistribute it and/or modify it
-// under the terms of the MIT License; see LICENSE file for more details.
+/*
+ * SPDX-FileCopyrightText: 2023 CERN.
+ * SPDX-License-Identifier: MIT
+ */
 
 /**
  * Add here all the overridden components of your app.
  */
 
-import { DepositBox } from "@js/invenio_curations/deposit/DepositBox";
-import { curationComponentOverrides } from "@js/invenio_curations/requests";
-
-export const overriddenComponents = {
-  ...curationComponentOverrides,
-  "InvenioAppRdm.Deposit.CardDepositStatusBox.container": DepositBox,
-};
+export const overriddenComponents = {}
